daf3a709cbe4f20ccc9a0cdf9dfe63c6b1a5ddb2
taa <trublovskaya.anna@agsr.by>
2025-12-03 17:44:31 +0300

    feat add micro

---
+++ course3/nest-project/gateway/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: 'USER_SERVICE',
+      useFactory: () =>
+        ClientProxyFactory.create({
+          transport: Transport.REDIS,
+          options: {
+            host: 'localhost',
+            port: 6379,
+          },
+        }),
+    },
+  ],
+})
+
+export class AppModule {}
